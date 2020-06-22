be6f371663c54d46a28297c115bc2bad11c233db
Tom Striker <toms.baugis@gmail.com>
2020-06-22 21:31:08 +0100

    fix the test

--- test/test.js
+++ test/test.js
@@ -3,24 +3,22 @@ const TextileTransformer = require("../index");
 
 describe("props and body", function () {
     it("get props", function () {
-        let source = `
-            The basic structure we expect is
-
-            [props]
+        let source = `---
             a: some
             b: props
             c:
                 - in yaml
                 - format
+            ---
 
-            [body]
-
-            Here's a [body] to add some confusion
+            Hello _textile_ content.
         `;
         let transformer = new TextileTransformer();
         assert.deepEqual(transformer.parse(source), {
-            props: {a: "some", b: "props", c: ["in yaml", "format"]},
-            body: "Here's a [body] to add some confusion",
+            a: "some",
+            b: "props",
+            c: ["in yaml", "format"],
+            body: "<p>Hello <em>textile</em> content.</p>",
         });
     });
 });
